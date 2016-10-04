9819a5c8a1a0bbdb9d0bbfa8c44e391b5dfbf525
Erika Bourque <erika.bourque@dawsoncollege.qc.ca>
2016-10-04 11:51:26 -0400

    Continued lab 5

---
+++ lab5/create.php
@@ -0,0 +1,24 @@
+<body>
+    <form action="">
+        <table>
+            <tr>
+                <td><label for="identifier">ID:</label></td>
+                <td><input type="text" id="identifer" name="productId"></td>
+            </tr>
+            <tr>
+                <td><label for="name">Name:</label></td>
+                <td><input type="text" id="name" name="productName"></td>
+            </tr>
+            <tr>
+                <td><label for="cost">Cost:</label></td>
+                <td><input type="text" id="cost" name="productCost"></td>
+            </tr>
+        </table>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+<?php
+    
+?>
+
--- lab5/index.php
+++ lab5/index.php
@@ -1,7 +1,27 @@
 <body>
-    <button value="submit">Create</button>
+    <a href="create.php">Create</a>
+    <a href="add.php">Add</a>
+    <a href="update.php">Update</a>
+    <a href="find.php">Find</a>
+    <?php
+        if (!empty($_GET['task']))
+        {
+            $task = $_GET['task'];
+            
+            if ($task == 'create')
+            {
+                echo "<p>The item was successfully created.</p>";
+            }
+            if ($task == 'delete')
+            {
+                echo "<p>The item was successfully deleted.</p>";
+            }
+            if ($task == 'update')
+            {
+                echo "<p>The item was successfully updated.</p>";
+            }
+        }
+    ?>
 </body>
-<?php
-    
-?>
+
 
